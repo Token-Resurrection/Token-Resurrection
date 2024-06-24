f7ad12d78ec62ebd5e94db75e6ac8b8e83975406
Princi <principatel1709@gmail.com>
2024-06-24 16:36:27 +0530

    onchainkit integration error

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import navstyle from "./navabr.module.css";
+// import { ConnectAccount } from '@coinbase/onchainkit/wallet'; 
 
 function Navbar() {
   return (
@@ -10,7 +11,7 @@ function Navbar() {
         </div>
         {/* <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"> */}
         <div className={navstyle.walletbtncontainer}>
-
+{/* <ConnectAccount/> */}
         <button className={navstyle.walletbtn}>
           Wallet Connect
         </button>
