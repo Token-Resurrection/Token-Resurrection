93dfb8129c340ed644ad8d51cb2bacaff9a381e6
Deepak973 <deepak.rj.dr@gmail.com>
2024-06-24 17:35:33 +0530

    solved wallet integration issue

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,20 +1,15 @@
-import React from 'react';
+import React from "react";
 import navstyle from "./navabr.module.css";
-// import { ConnectAccount } from '@coinbase/onchainkit/wallet'; 
-
+import { ConnectAccount } from "@coinbase/onchainkit/wallet";
 function Navbar() {
   return (
     <div className=" p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-black text-2xl font-bold">
-          Token Resurrection
-        </div>
+        <div className="text-black text-2xl font-bold">Token Resurrection</div>
         {/* <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700"> */}
         <div className={navstyle.walletbtncontainer}>
-{/* <ConnectAccount/> */}
-        <button className={navstyle.walletbtn}>
-          Wallet Connect
-        </button>
+          <ConnectAccount className={navstyle.walletbtn} />
+          <button className={navstyle.walletbtn}>Wallet Connect</button>
         </div>
       </div>
     </div>
