2a5a14280b2cb20acabd6117d81bb8b22b482812
Princi <principatel1709@gmail.com>
2024-06-27 12:08:09 +0530

    Custom Wallet added

--- src/Components/Navbar/ConnectAccount.js
+++ src/Components/Navbar/ConnectAccount.js
@@ -1,23 +1,41 @@
-import { Avatar } from '@coinbase/onchainkit/identity';
 import { ConnectAccount } from '@coinbase/onchainkit/wallet'; 
 import { useAccount, useDisconnect } from 'wagmi';
- 
+import style from './navabr.module.css'; // Ensure you have this CSS file
+import { Tooltip, message } from 'antd';
+
 function AccountConnect() {
   const { address, status } = useAccount();
   const { disconnect } = useDisconnect();
- 
+
+  const formatAddress = (addr) => {
+    if (!addr) return '';
+    return `${addr.slice(0, 5)}...${addr.slice(-5)}`;
+  };
+
+  const handleDisconnect = () => {
+    disconnect();
+    message.success('Wallet Disconnected!');
+  };
+
   return (
     <div className="flex flex-grow">
       {(() => {
         if (status === 'disconnected') {
-          return <ConnectAccount />; 
+          return  <ConnectAccount />
+          
         }
         return (
-          <div className="flex h-8 w-8 items-center justify-center">
+          <div className={`flex h-8 items-center justify-center ${style.dynamicWidth}`}>
             {address && (
-              <button type="button" onClick={() => disconnect()}>
-                <Avatar address={address} />
+                <Tooltip title="Click to Disconnect">
+              <button 
+                type="button" 
+                onClick={handleDisconnect}
+                style={{ minWidth: `${formatAddress(address).length * 10}px` }} // Adjust min-width based on address length
+                >
+                {formatAddress(address)}
               </button>
+                  </Tooltip>
             )}
           </div>
         );
@@ -25,3 +43,5 @@ function AccountConnect() {
     </div>
   );
 }
+
+export default AccountConnect;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -2,6 +2,8 @@ import React from "react";
 import { ConnectAccount } from "@coinbase/onchainkit/wallet";
 import '@coinbase/onchainkit/styles.css';
 import style from './navabr.module.css'; // Import your custom styles
+import AccountConnect from "./ConnectAccount";
+// import ConnectAccountWrapper from "./Connectwrapperaccount";
 
 function Navbar() {
   return (
@@ -10,7 +12,9 @@ function Navbar() {
         <div className="text-black text-2xl font-bold">Token Resurrection</div>
         <div className={style.walletbtncontainer}>
           <div className={style.walletbtn}>
-            <ConnectAccount />
+            {/* <ConnectAccount /> */}
+              <AccountConnect/>
+            {/* <ConnectAccountWrapper/> */}
           </div>
         </div>
       </div>
